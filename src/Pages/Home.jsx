62499ddf1c3c2fceb0cb9eb2f53238c8e50da715
UR Raju <riadhasanraju>
2023-12-30 21:46:35 +0600

    navbar2 section complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../Layout/Navbar";
 import NavbarHead from "./NavbarHead";
 
 const Home = () => {
     return(
         <div>
              <NavbarHead/>
+             <Navbar/>
         </div>
     )}
 export default Home;
